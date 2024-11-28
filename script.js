5a815abab2ca7bb4183fd06376dd17711cf64f27
Philip Yaw Boahene <boahenep22@gmail.com>
2024-11-27 20:21:43 -0800

    ajex exe complete

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-$(function() {
+$(function () {
   // your code here
-})
+  $(".accordion h3").on("click", function () {
+    $(".accordion-content").not($(this).next("div")).slideUp();
+    $(this).next("div").slideToggle();
+  });
+
+  $(function () {
+    $(".todos button").on("click", function () {
+      $.ajax({
+        url: "https://dummyjson.com/todos",
+        method: "GET",
+        success: function (response) {
+          $(".todos ul").empty();
+          response.todos.forEach(function (todo) {
+            $(".todos ul").append(`<li>${todo.todo}</li>`);
+          });
+        },
+        error: function () {
+          alert("An error occurred while loading todos.");
+        },
+      });
+    });
+  });
+});
